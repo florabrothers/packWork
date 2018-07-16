3c9500238c9482249c35d3da3c01b02d0484a8f9
Benson <bensonliu1990@hotmail.com>
2018-07-17 07:31:07 +0900

    registration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -25,7 +26,8 @@ import { SetsComponent } from './workbench/sets/sets.component';
     BrowserModule,
     AppRoutes,
     ClarityModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -1,20 +1,64 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ClrWizard } from '@clr/angular';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ClrWizard, ClrStackBlock } from '@clr/angular';
+import { trigger, transition, animate, style } from '@angular/animations'
 
 @Component({
   selector: 'app-community',
   templateUrl: './community.component.html',
-  styleUrls: ['./community.component.css']
+  styleUrls: ['./community.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)', opacity: 0 }),
+        animate('200ms ease-in', style({ transform: 'translateX(0%)', opacity: 1}))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({ transform: 'translateX(-100%)', opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class CommunityComponent implements OnInit {
-  @ViewChild('wizardlg') wizardLarge: ClrWizard;
+  @ViewChild('wizardmd') wizardmedium: ClrWizard;
+  @ViewChild('firstBlock') firstBlock: ClrStackBlock;
+  @ViewChild('secondBlock') secondBlock: ClrStackBlock;
 
-  lgOpen: boolean = false;
+  open: boolean = true;
 
+  public model = {
+    content1: "old1",
+    content2: "old2"
+  }
 
-  constructor() { }
+  modelNew = {
+    content1: "",
+    content2: ""
+  }
+
+  constructor(private el: ElementRef) { }
 
   ngOnInit() {
+  }
+
+  openFirst() {
+    if (this.firstBlock.expanded === true) {
+      this.firstBlock.expanded = false;
+    } else {
+      this.firstBlock.expanded = true;
+      this.secondBlock.expanded = false;
+      this.el.nativeElement.querySelector('.first-input').focus();
+    }
+  }
+  openSecond() {
+    if (this.secondBlock.expanded === true) {
+      this.secondBlock.expanded = false;
+    } else {
+      this.secondBlock.expanded = true;
+      this.firstBlock.expanded = false;
+      this.el.nativeElement.querySelector('.second-input').focus();
+
+    }
+
   }
 
 }
