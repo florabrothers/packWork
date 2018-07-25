da99679127df4d986d5d6da63e09ca75f87179e0
Naganeo Benson <naganeo@NaganeonoMacBook-puro.local>
2018-07-25 18:25:21 +0900

    katext improvement

--- src/app/workbench/problems/problem/problem.component.ts
+++ src/app/workbench/problems/problem/problem.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { KatexOptions } from 'ng-katex';
 
 @Component({
   selector: 'app-problem',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./problem.component.scss']
 })
 export class ProblemComponent implements OnInit {
+
+  equation = String.raw`\begin{alignedat}{2}
+   10&x+ &3&y = 2 \\
+    3&x+&13&y = 4
+\end{alignedat}`;
+  options: KatexOptions = {
+    displayMode: true
+  };
 
   constructor() { }
 
